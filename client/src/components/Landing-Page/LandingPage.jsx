5417bb27c192e6108c6ff500d19c60a403f97088
JotaCe <jcvetromile@gmail.com>
2024-02-26 18:47:02 -0300

    early landing page

--- client/src/components/Landing-Page/LandingPage.jsx
+++ client/src/components/Landing-Page/LandingPage.jsx
@@ -1,10 +1,34 @@
+import { Center, Flex, Heading } from "@chakra-ui/react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
-  // milseis color:#f5d1b7
-  // tiente color: #7bd0c9
+  const navigate = useNavigate();
 
-  return <div>LandingPage</div>;
+  return (
+    <Flex>
+      <Center bg="#7bd0c9" w="50%" h="100vh">
+        <Heading
+          transition="fontSize 0.3s ease-in-out"
+          _hover={{ fontSize: "5xl" }}
+          userSelect="none"
+          onClick={() => navigate("/Tiente")}
+        >
+          Tiente
+        </Heading>
+      </Center>
+      <Center bg="#f5d1b7" w="50%" h="100vh">
+        <Heading
+          transition="fontSize 0.3s ease-in-out"
+          _hover={{ fontSize: "5xl" }}
+          userSelect="none"
+          onClick={() => navigate("/Milseis")}
+        >
+          MilSeis
+        </Heading>
+      </Center>
+    </Flex>
+  );
 }
 
 export default LandingPage;
